0de2c90cf078906bf0557d28932e2da496b5971d
CristianMaschio <blackspartan@hotmail.it>
2019-10-22 10:30:00 +0100

    [app] add test

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { mount } from "enzyme";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("<App/>", () => {
+  let component;
+
+  it("should render correctly", () => {
+    component = mount(<App />);
+  });
+
+  describe("first step", () => {
+    it("should render all inputs", () => {
+      expect(component.find("#first-name").exists()).toEqual(true);
+      expect(component.find("#last-name").exists()).toEqual(true);
+      expect(component.find("#email").exists()).toEqual(true);
+    });
+
+    it("should inputs required", () => {
+      expect(
+        component
+          .find("#first-name")
+          .at(0)
+          .props()["required"]
+      ).toBe(true);
+      expect(
+        component
+          .find("#last-name")
+          .at(0)
+          .props()["required"]
+      ).toBe(true);
+      expect(
+        component
+          .find("#email")
+          .at(0)
+          .props()["required"]
+      ).toBe(true);
+    });
+  });
+
+  describe("Second step", () => {
+    it("should render all inputs", () => {
+      expect(component.find("#mobile-number").exists()).toEqual(true);
+      expect(component.find("#gender").exists()).toEqual(true);
+      expect(component.find("#day-birth").exists()).toEqual(true);
+      expect(component.find("#month-birth").exists()).toEqual(true);
+      expect(component.find("#year-birth").exists()).toEqual(true);
+    });
+  });
+
+  describe("Third step", () => {
+    it("should render all inputs", () => {
+      expect(component.find("#comments").exists()).toEqual(true);
+    });
+  });
 });
